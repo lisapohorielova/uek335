cb67a0fce1046feb8741fadd8b881f6cd809eb90
lisapohorielova <lisa.pohorielova@noseryoung.com>
2026-05-18 11:00:00 +0200

    init

---
+++ uek335/src/app/App.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import { Text } from "react-native";
+import { PaperProvider } from "react-native-paper";
+import { SafeAreaView } from "react-native-safe-area-context";
+export default function App() {
+    return (
+        <PaperProvider>
+            <SafeAreaView>
+                <Text>Hello World</Text>
+            </SafeAreaView>
+        </PaperProvider>
+    );
+}
---
+++ uek335/src/app/_layout.tsx
@@ -0,0 +1,19 @@
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import React from 'react';
+import { useColorScheme } from 'react-native';
+
+import { AnimatedSplashOverlay } from '@/components/animated-icon';
+import AppTabs from '@/components/app-tabs';
+import { PaperProvider} from "react-native-paper";
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+  return (
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <PaperProvider>
+      <AnimatedSplashOverlay />
+      <AppTabs />
+        </PaperProvider>
+    </ThemeProvider>
+  );
+}
