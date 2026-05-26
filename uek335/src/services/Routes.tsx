473ecd8f98849bd314a94e18078af458a1e5bfc7
lisapohorielova <lisa.pohorielova@noseryoung.com>
2026-05-26 12:20:05 +0200

    ADDED: Login functionality; created LoginPage, LoginPageTmpl, and LoginFormData interface; integrated API calls for user authentication

--- uek335/src/services/Routes.tsx
+++ uek335/src/services/Routes.tsx
@@ -1,3 +1,4 @@
 
 export const BasePath = 'http://localhost:3030'
 export const RegisterEndpoint = '/signup'
+export const LoginEndpoint = '/login'
--- uek335/src/types/RegisterFormData.tsx
+++ uek335/src/types/RegisterFormData.tsx
@@ -1,4 +1,4 @@
-export interface FormData {
+export interface RegisterFormData {
     firstName: string;
     lastName: string;
     email: string;
